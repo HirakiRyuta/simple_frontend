9e0828bc39c37d060254109230523dab659e79d2
hirakiryuta <hirakiryuta@team-lab.com>
2019-08-25 13:43:53 +0900

    MyButton

---
+++ src/components/MyButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="onClick">
+    <slot>遊ぼう</slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Emit } from "vue-property-decorator";
+
+@Component
+export default class MyButton extends Vue {
+  private count: number = 0;
+
+  @Prop()
+  public greet?: string;
+
+  @Emit()
+  public clicked(count: number) {}
+
+  public onClick() {
+    this.count++;
+    this.clicked(this.count);
+  }
+}
+</script>
