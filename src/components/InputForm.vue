0856f05c10eb0760ffdec9f325067b86b2e31891
hirakiryuta <hirakiryuta@team-lab.com>
2019-08-25 13:37:40 +0900

    form実装開始

---
+++ src/components/InputForm.vue
@@ -0,0 +1,12 @@
+<template></template>
+
+<script>
+import { Component } from 'vue-property-decorator';
+@Component
+export default {
+  
+}
+</script>
+
+<style scoped lang="scss">
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
+const PrettierPlugin = require('prettier-webpack-plugin');
+
 module.exports = {
   devServer: {
     port: 4000
+  },
+  configureWebpack: {
+    plugins: [
+      new PrettierPlugin({
+        singleQuote: true,
+        semi: false,
+        tabWidth: 2,
+        printWidth: 200
+      })
+    ]
   }
 };
